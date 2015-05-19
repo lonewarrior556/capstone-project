23d4865bab7930b91a29b366ad5ed2101d3c43d8
App Academy Student <appacademy@Apps-Mac-mini-3.local>
2015-05-19 15:20:54 -0400

    test goes on 5

--- app/assets/javascripts/models/user.js
+++ app/assets/javascripts/models/user.js
@@ -3,16 +3,16 @@ CapstoneProject.Models.User = Backbone.Model.extend({
 
   parse: function(data){
     data = data.user
-    this.askedQuestions = new CapstoneProject.Collections.IndexQuestions(data.questions)
-    this.answeredQuestions = new CapstoneProject.Collections.IndexQuestions(data.answers)
+    this.askedQuestions = new CapstoneProject.Collections.IndexQuestions(data.questions);
+    this.answeredQuestions = new CapstoneProject.Collections.IndexQuestions(data.answers);
 
-    delete data.questions
-    delete data.answers
+    delete data.questions;
+    delete data.answers;
 
     return data
   }
 
-})
+});
 
 CapstoneProject.Models.indexUser = Backbone.Model.extend({
 
