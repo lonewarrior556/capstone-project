00b3809451e411877ed3a859fa7ecf63b96cb479
App Academy Student <appacademy@Apps-Mac-mini-3.local>
2015-05-19 15:03:42 -0400

    looking for error

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,14 +10,14 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
-//= require jquery_ujs
-//= require underscore
-//= require backbone
-//= require_tree ./utils
-//= require capstone_project
-//= require_tree ../templates
-//= require_tree ./models
-//= require_tree ./collections
-//= require_tree ./routers
-//= require_tree ./views
+// require jquery
+// require jquery_ujs
+// require underscore
+// require backbone
+// require_tree ./utils
+// require capstone_project
+// require_tree ../templates
+// require_tree ./models
+// require_tree ./collections
+// require_tree ./routers
+// require_tree ./views
