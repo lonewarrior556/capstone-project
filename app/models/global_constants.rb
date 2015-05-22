31b4230a0d578159983a7c971b49820067898929
App Academy Student <appacademy@Apps-Mac-mini-4.local>
2015-05-22 11:08:59 -0400

    script testing in login

---
+++ app/models/global_constants.rb
@@ -0,0 +1,11 @@
+module GlobalConstants
+
+  @all = "Alex Haas,https://github.com/haasalex16/CapstoneProject,|,Brian Kang,https://github.com/Kayleith/shutter-step,|,Charles Chen,https://github.com/chalzstrassen/Capstone-Project,|,Dylan Nelson,https://github.com/dylanonelson/medium_spare,|,Emmanuel Genard,https://github.com/edgenard/study_two,|,Eric Firth,https://github.com/ericfirth/quack-app,|,Eric Savadian,https://github.com/slancio/meeplespace,|,Felicia Wong,https://github.com/quixotically/Airfnf,|,Harris Jordan,https://github.com/wjordan213/BetterNote,|,Jeremy Lilly,https://github.com/giovanni-da-rumore/cicadas-song,|,Jose Garza,https://github.com/minutetominute/mysuperheropal,|,Konrad Dudziak,https://github.com/lonewarrior556/capstone-project,|,Liann Sun,https://github.com/lisunshiny/welp,|,Lily Riopelle,https://github.com/lilyriopelle/cooking_genius/,|,Lily Zhang,https://github.com/lilyxl8/leppo,|,Nathan Hinchey,https://github.com/nathanhinchey/okcupid_clone,|,Rhoen Pruesse-Adams,https://github.com/rhoen/nycBNB,|,Sung Jin Kim,https://github.com/sjkim24/questiontoallanswers,|,Thomas Jenkins,https://github.com/Arcane52689/Classic-Car-Classifieds/,|,Thomas Paresi,https://github.com/tparesi/product-hunt".gsub(",|,","|")
+
+
+  ALL_PROJECTS = @all.split("|").map{|a| a.split(",")}
+
+
+
+
+end
